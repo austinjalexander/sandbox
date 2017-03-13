814fa6b8f5e48989d7daebb29ba3fd266a1ab37a
Austin J. Alexander <austinjalexander@gmail.com>
2017-03-13 00:52:04 -0700

    Check in

--- cpp/sha1/sha1.cpp
+++ cpp/sha1/sha1.cpp
@@ -1,8 +1,13 @@
 // % clang++ sha1.cpp -Weverything -o bin/sha1 && ./bin/sha1
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    "The quick brown fox jumps over the lazy dog";
+	string message = "The quick brown fox jumps over the lazy dog";
+    cout << "message: " << message << endl;
+    cout << "string length: " <<  message.length() << endl;
 
     /* 
      * Note 1: All variables are unsigned 32-bit quantities and wrap modulo 2^32 when calculating, excet for
@@ -12,6 +17,13 @@ int main() {
      * Within each word, the most significant byte is stored in the leftmost byte position.
      */
 
-    int h0; 
+    /*
+    int h0 = 0x67452301;
+    int h1 = 0xEFCDAB89;
+    int h2 = 0x98BADCFE;
+    int h3 = 0x10325476;
+    int h4 = 0xC3D2E1F0;
+    */
 
+    m1 
 }
